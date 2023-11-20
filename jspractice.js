7441230c3cc1ef332adaf3381aed69f4200889a6
Craigm84 <craigmorris2907@gmail.com>
2023-11-20 11:56:34 +0000

    init JS commit

---
+++ jspractice.js
@@ -0,0 +1,46 @@
+let myname = "Craig"
+
+console.log(myname)
+
+myname = "Not Craig"
+
+console.log(myname)
+
+let date= 20
+
+console.log(date)
+
+date = "21"
+
+console.log(date)
+
+let learning = true
+
+console.log(learning)
+
+learning = false
+
+console.log(learning)
+
+let surname = null
+
+console.log(surname)
+
+let middlename;
+
+console.log(middlename)
+
+console.log(typeof date)
+
+console.log(typeof myname)
+
+const hername = "Paige"
+
+console.log(hername)
+
+hername = "Adele"
+
+console.log(hername)
+
+
+
